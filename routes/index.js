18b6d32c9fc74b206e1b9a3c0cbebb12ff71c488
Micah Bales <micahbales@gmail.com>
2017-06-12 15:20:12 -0400

    saving stores and using mixins

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const storeController = require('../controllers/storeController');
 
 // Do work here
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
