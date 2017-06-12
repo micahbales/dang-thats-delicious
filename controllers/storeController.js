81a114145905f241192e94f3c165589662272ee1
Micah Bales <micahbales@gmail.com>
2017-06-12 14:11:39 -0400

    core concept - middleware and error handling

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,12 @@
+exports.myMiddleware = (req, res, next) => {
+  req.name = 'Micah';
+  if (req.name = 'Micah') {
+    throw Error('That is a stupid name');
+  }
+  next();
+}
+
 exports.homePage = (req, res) => {
+  console.log(req.name);
   res.render('index');
 }
