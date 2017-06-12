49bc75c762fb842cd389fcc461fbfc13a4fb70ee
Micah Bales <micahbales@gmail.com>
2017-06-12 12:45:36 -0400

    core concept - routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  // const micah = { name: 'Bob', age: 43, cool: true };
+  // res.json(micah);
+  res.json(req.query)
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
