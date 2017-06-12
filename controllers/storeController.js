05e24881c49a3d98847161e25bb60d7e598b2bdf
Micah Bales <micahbales@gmail.com>
2017-06-12 14:27:52 -0400

    creating our store model

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,11 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Micah';
-  if (req.name = 'Micah') {
-    throw Error('That is a stupid name');
-  }
-  next();
-}
-
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
