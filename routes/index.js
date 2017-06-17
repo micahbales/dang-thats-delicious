83f548ccbf75093ba5a51700dd48d70e6860a58b
Micah Bales <micahbales@gmail.com>
2017-06-16 22:29:35 -0400

    routing and templating single stores

--- routes/index.js
+++ routes/index.js
@@ -19,11 +19,8 @@ router.post('/add/:id',
   catchErrors(storeController.updateStore)
 );
 
-router.get('/stores/:id/edit', catchErrors(storeController.editStore))
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 module.exports = router;
