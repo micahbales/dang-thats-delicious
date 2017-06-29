152e6acdc0c6773fee2e98fc1545416a14b9ee38
Micah Bales <micahbales@gmail.com>
2017-06-29 18:22:49 -0400

    use postmark to send password reset emails

--- handlers/mail.js
+++ handlers/mail.js
@@ -1,17 +1,9 @@
-const nodemailer = require('nodemailer');
 const pug = require('pug');
 const juice = require('juice');
 const htmlToText = require('html-to-text');
 const promisify = require('es6-promisify');
-
-const transport = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  }
-});
+const postmark = require("postmark");
+const client = new postmark.Client(process.env.POSTMARK_API);
 
 const generateHTML = (filename, options = {}) => {
   // convert pug files into regular HTML
@@ -25,13 +17,13 @@ exports.send = async (options) => {
   const html = generateHTML(options.filename, options);
   const text = htmlToText.fromString(html);
   const mailOptions = {
-    from: 'Micah Bales <micahbales@gmail.com>',
-    to: options.user.email,
-    subject: options.subject,
-    html,
-    text
+    "From": "Micah Bales <micah@micahbales.com>",
+    "To": options.user.email,
+    "Subject": options.subject,
+    "HtmlBody": html,
+    "TextBody": text
   }
-  const sendMail = promisify(transport.sendMail, transport);
+  const sendEmail = promisify(client.sendEmail, client);
 
-  return sendMail(mailOptions);
+  return sendEmail(mailOptions);
 };
